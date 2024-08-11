3bbb9f8134e2592b55ad2f6ca4043040f6707719
vbevz <122693446+vladbevz@users.noreply.github.com>
2024-08-11 16:58:38 +0200

    commit

---
+++ src/components/App/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+
+import css from './Profile.module.css'
+
+export default function Profile ({username, tag, location, avatar, stats}) {
+    return <div className={css.container}>
+    <div >
+      <img className={css.avatar}
+        src={avatar}
+        alt="User avatar"
+      />
+      <p className={css.username}>{username}</p>
+      <p className={css.tag}>{tag}</p>
+      <p className={css.location}>{location}</p>
+    </div>
+  
+    <ul className={css.list}>
+      <li className={css.listitem}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{stats.followers}</span>
+      </li>
+      <li className={css.listitem}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{stats.views}</span>
+      </li>
+      <li className={css.listitem}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App/App.jsx'
+import './index.module.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    
+  </StrictMode>,
+)
